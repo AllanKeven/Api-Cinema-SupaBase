a31534ade6724a97d75cabe81c7f8b58b672f14b
ALLAN KEVEN ALENCAR MESQUITA <allanmesquita6263@digitalcollege.internal>
2025-09-29 21:48:57 -0300

    Get and Post movies

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors')
 const app = express()
 const sequelize = require('./config/database');
 const User = require('./models/User');
 const Movies = require('./models/Movies');
+const { json } = require('sequelize');
 const port = 3000
+
+app.use(cors());
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/movies', (req, res) => {
-    res.send('Movies List')
+app.get('/movies', async (req, res) => {
+    try {
+        const movies = await Movies.findAll();
+
+        res.status(200).json(movies);
+    } catch (error) {
+        console.error(error)
+        res.status(500).json(error);
+    }
 })
 
-app.post('/movies', (req, res) => {
-    res.send('Add a Movie')
+app.post('/movies', async(req, res) => {
+    try {
+        
+        await Movies.create(req.body);
+        res.status(201).json({message: "Filme criado com sucesso!"});
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error:"Erro detectado"})        
+    }
 })
 
 app.put('/movies/:id', (req, res) => {
